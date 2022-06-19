6da18415b66ea8fe621fac5b92d5cb6416e97a2f
mskhrk7101 <msk19970709@gmail.com>
2022-06-19 18:40:29 +0900

    Second Commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication
-//@RestController
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
